4996b7d795c2be8badbd4ff6590c61609481b0e3
LeonPu2017 <liwangp2012@163.com>
2017-12-13 00:15:16 +0800

    add fenku

---
+++ YiPin_Apps/Gadget/_分库盘点.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Gadget
+{
+    partial class _分库盘点
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt盘点结果 = new System.Windows.Forms.TextBox();
+            this.cb只要盘点结果 = new System.Windows.Forms.CheckBox();
+            this.nd遗留上海天数 = new System.Windows.Forms.NumericUpDown();
+            this.lkDecs = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.txtExport = new System.Windows.Forms.TextBox();
+            this.lbMsg = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn盘点结果 = new System.Windows.Forms.Button();
+            this.btn库存明细 = new System.Windows.Forms.Button();
+            this.txt库存明细 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nd遗留上海天数)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt盘点结果);
+            this.groupBox1.Controls.Add(this.cb只要盘点结果);
+            this.groupBox1.Controls.Add(this.nd遗留上海天数);
+            this.groupBox1.Controls.Add(this.lkDecs);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.btnAnalyze);
+            this.groupBox1.Controls.Add(this.txtExport);
+            this.groupBox1.Controls.Add(this.lbMsg);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn盘点结果);
+            this.groupBox1.Controls.Add(this.btn库存明细);
+            this.groupBox1.Controls.Add(this.txt库存明细);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(357, 166);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "数据上传";
+            // 
+            // txt盘点结果
+            // 
+            this.txt盘点结果.Enabled = false;
+            this.txt盘点结果.Location = new System.Drawing.Point(75, 48);
+            this.txt盘点结果.Name = "txt盘点结果";
+            this.txt盘点结果.Size = new System.Drawing.Size(183, 21);
+            this.txt盘点结果.TabIndex = 23;
+            // 
+            // cb只要盘点结果
+            // 
+            this.cb只要盘点结果.AutoSize = true;
+            this.cb只要盘点结果.Location = new System.Drawing.Point(243, 114);
+            this.cb只要盘点结果.Name = "cb只要盘点结果";
+            this.cb只要盘点结果.Size = new System.Drawing.Size(96, 16);
+            this.cb只要盘点结果.TabIndex = 22;
+            this.cb只要盘点结果.Text = "只要盘点结果";
+            this.cb只要盘点结果.UseVisualStyleBackColor = true;
+            // 
+            // nd遗留上海天数
+            // 
+            this.nd遗留上海天数.Location = new System.Drawing.Point(108, 112);
+            this.nd遗留上海天数.Name = "nd遗留上海天数";
+            this.nd遗留上海天数.Size = new System.Drawing.Size(75, 21);
+            this.nd遗留上海天数.TabIndex = 21;
+            this.nd遗留上海天数.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // lkDecs
+            // 
+            this.lkDecs.AutoSize = true;
+            this.lkDecs.Location = new System.Drawing.Point(286, 145);
+            this.lkDecs.Name = "lkDecs";
+            this.lkDecs.Size = new System.Drawing.Size(53, 12);
+            this.lkDecs.TabIndex = 20;
+            this.lkDecs.TabStop = true;
+            this.lkDecs.Text = "表格说明";
+            this.lkDecs.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lkDecs_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "天";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "预留(上海)天数:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "盘点结果:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "库存明细:";
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(264, 79);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyze.TabIndex = 5;
+            this.btnAnalyze.Text = "处理";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // txtExport
+            // 
+            this.txtExport.Location = new System.Drawing.Point(75, 81);
+            this.txtExport.Name = "txtExport";
+            this.txtExport.Size = new System.Drawing.Size(183, 21);
+            this.txtExport.TabIndex = 4;
+            // 
+            // lbMsg
+            // 
+            this.lbMsg.AutoSize = true;
+            this.lbMsg.Location = new System.Drawing.Point(73, 145);
+            this.lbMsg.Name = "lbMsg";
+            this.lbMsg.Size = new System.Drawing.Size(65, 12);
+            this.lbMsg.TabIndex = 3;
+            this.lbMsg.Text = "待上传文件";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "操作消息:";
+            // 
+            // btn盘点结果
+            // 
+            this.btn盘点结果.Location = new System.Drawing.Point(264, 47);
+            this.btn盘点结果.Name = "btn盘点结果";
+            this.btn盘点结果.Size = new System.Drawing.Size(75, 23);
+            this.btn盘点结果.TabIndex = 1;
+            this.btn盘点结果.Text = "浏览";
+            this.btn盘点结果.UseVisualStyleBackColor = true;
+            this.btn盘点结果.Click += new System.EventHandler(this.btn盘点结果_Click);
+            // 
+            // btn库存明细
+            // 
+            this.btn库存明细.Location = new System.Drawing.Point(264, 18);
+            this.btn库存明细.Name = "btn库存明细";
+            this.btn库存明细.Size = new System.Drawing.Size(75, 23);
+            this.btn库存明细.TabIndex = 1;
+            this.btn库存明细.Text = "浏览";
+            this.btn库存明细.UseVisualStyleBackColor = true;
+            this.btn库存明细.Click += new System.EventHandler(this.btn库存明细_Click);
+            // 
+            // txt库存明细
+            // 
+            this.txt库存明细.Enabled = false;
+            this.txt库存明细.Location = new System.Drawing.Point(75, 20);
+            this.txt库存明细.Name = "txt库存明细";
+            this.txt库存明细.Size = new System.Drawing.Size(183, 21);
+            this.txt库存明细.TabIndex = 0;
+            // 
+            // _分库盘点
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 184);
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(390, 222);
+            this.MinimumSize = new System.Drawing.Size(390, 222);
+            this.Name = "_分库盘点";
+            this.Text = "_分库盘点";
+            this.Load += new System.EventHandler(this._分库盘点_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nd遗留上海天数)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.LinkLabel lkDecs;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Label lbMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn库存明细;
+        private System.Windows.Forms.TextBox txt库存明细;
+        private System.Windows.Forms.TextBox txtExport;
+        private System.Windows.Forms.Button btn盘点结果;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cb只要盘点结果;
+        private System.Windows.Forms.NumericUpDown nd遗留上海天数;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt盘点结果;
+
+
+
+    }
+}
---
+++ YiPin_Apps/Gadget/_分库盘点.cs
@@ -0,0 +1,510 @@
+﻿using LinqToExcel;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using CommonLibs;
+using LinqToExcel.Attributes;
+
+namespace Gadget
+{
+    public partial class _分库盘点 : Form
+    {
+        public _分库盘点()
+        {
+            InitializeComponent();
+        }
+
+        private void _分库盘点_Load(object sender, EventArgs e)
+        {
+            //txt库存明细.Text = @"C:\Users\Leon\Desktop\所有商品信息.csv";
+            //txt盘点结果.Text = @"C:\Users\Leon\Desktop\盘点.csv";
+        }
+
+        /**************** button event ****************/
+
+        #region 上传库存明细
+        private void btn库存明细_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.Filter = "CSV 文件|*.csv";//设置文件类型
+            OpenFileDialog1.Title = "CSV 文件";//设置标题
+            OpenFileDialog1.Multiselect = false;
+            OpenFileDialog1.AutoUpgradeEnabled = true;//是否随系统升级而升级外观
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)//如果点的是确定就得到文件路径
+            {
+                if (Helper.CheckCSVFileName(OpenFileDialog1.FileName))
+                {
+                    txt库存明细.Text = OpenFileDialog1.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("csv文件名称不规范,请去掉文件名称中的特殊字符如\".\"等", "温馨提示");
+                }
+            }
+        }
+        #endregion
+
+        #region 上传盘点结果
+        private void btn盘点结果_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 = new OpenFileDialog();
+            OpenFileDialog1.Filter = "CSV 文件|*.csv";//设置文件类型
+            OpenFileDialog1.Title = "CSV 文件";//设置标题
+            OpenFileDialog1.Multiselect = false;
+            OpenFileDialog1.AutoUpgradeEnabled = true;//是否随系统升级而升级外观
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)//如果点的是确定就得到文件路径
+            {
+                if (Helper.CheckCSVFileName(OpenFileDialog1.FileName))
+                {
+                    txt盘点结果.Text = OpenFileDialog1.FileName;
+                }
+                else
+                {
+                    MessageBox.Show("csv文件名称不规范,请去掉文件名称中的特殊字符如\".\"等", "温馨提示");
+                }
+            }
+        }
+        #endregion
+
+        #region 处理数据
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            var list库存明细 = new List<_库存明细Mapping>();
+            var list盘点结果 = new List<_盘点表Mapping>();
+            var list导出结果 = new List<_分析结果Model>();
+            var i遗留上海天数 = nd遗留上海天数.Value;
+            var b只取盘点对应 = cb只要盘点结果.Checked;
+
+            ShowMsg("开始读取表格信息");
+
+            #region 读取数据
+            var actReadData = new Action(() =>
+            {
+                #region 读取库存明细
+                {
+                    var strCSVPath = txt库存明细.Text;
+                    if (!string.IsNullOrEmpty(strCSVPath))
+                    {
+                        using (var csv = new ExcelQueryFactory(strCSVPath))
+                        {
+                            try
+                            {
+                                var tmp = from c in csv.Worksheet<_库存明细Mapping>()
+                                          select c;
+                                list库存明细.AddRange(tmp);
+                            }
+                            catch (Exception ex)
+                            {
+                                ShowMsg(ex.Message);
+                            }
+                        }
+                    }
+                }
+                #endregion
+
+                #region 读取盘点表
+                {
+                    var strCSVPath = txt盘点结果.Text;
+                    if (!string.IsNullOrEmpty(strCSVPath))
+                    {
+                        using (var csv = new ExcelQueryFactory(strCSVPath))
+                        {
+                            try
+                            {
+                                var tmp = from c in csv.Worksheet<_盘点表Mapping>()
+                                          select c;
+                                list盘点结果.AddRange(tmp);
+                            }
+                            catch (Exception ex)
+                            {
+                                ShowMsg(ex.Message);
+                            }
+                        }
+                    }
+                }
+                #endregion
+            });
+            #endregion
+
+            ShowMsg("开始处理数据");
+
+            #region 处理数据
+            actReadData.BeginInvoke((obj) =>
+            {
+                list库存明细.ForEach(dtItem =>
+                {
+                    //if (dtItem.SKU=="DNFD12D46-S2")
+                    //{
+                        
+                    //}
+
+                    var model = new _分析结果Model();
+                    model._SKU = dtItem.SKU;
+                    model._商品名称 = dtItem._商品名称;
+                    model._库位 = dtItem._库位;
+                    model._库存数量 = dtItem._库存数量;
+                    model._日平均销量 = dtItem._平均日销量;
+                    model._遗留上海天数 = i遗留上海天数;
+
+                    var ref盘点Item = list盘点结果.Where(x => x.SKU == dtItem.SKU).FirstOrDefault();
+                    if (ref盘点Item != null)
+                    {
+                        model._盘点数量 = ref盘点Item._数量;
+                    }
+
+                    if (b只取盘点对应)
+                    {
+                        if (ref盘点Item != null)
+                        {
+                            list导出结果.Add(model);
+                        }
+                    }
+                    else
+                    {
+                        list导出结果.Add(model);
+                    }
+                });
+
+                Export(list导出结果.OrderBy(x => x._库位).ToList());
+            }, null);
+            #endregion
+        }
+        #endregion
+
+        #region 导出表格说明
+        private void lkDecs_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var strDesc = XlsxHelper.GetDecsipt(typeof(_库存明细Mapping), typeof(_盘点表Mapping));
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "记事本|*.txt";//设置文件类型
+            saveFile.Title = "导出说明文件";//设置标题
+            saveFile.AddExtension = true;//是否自动增加所辍名
+            saveFile.AutoUpgradeEnabled = true;//是否随系统升级而升级外观
+            if (saveFile.ShowDialog() == DialogResult.OK)//如果点的是确定就得到文件路径
+            {
+                File.WriteAllText(saveFile.FileName, strDesc);
+            }
+        }
+        #endregion
+
+        /**************** common method ****************/
+
+        #region Export 导出结果表格
+        private void Export(List<_分析结果Model> list)
+        {
+            ShowMsg("开始生成表格");
+            var buffer = new byte[0];
+
+            #region 数据表
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                var workbox = package.Workbook;
+
+                #region 汇总表
+                {
+                    var sheet1 = workbox.Worksheets.Add("所有");
+
+                    #region 标题行
+                    sheet1.Cells[1, 1].Value = "SKU";
+                    sheet1.Cells[1, 2].Value = "商品名称";
+                    sheet1.Cells[1, 3].Value = "库位";
+                    sheet1.Cells[1, 4].Value = "仓库";
+                    sheet1.Cells[1, 5].Value = "货架";
+                    sheet1.Cells[1, 6].Value = "库存数量";
+                    sheet1.Cells[1, 7].Value = "盘点数量";
+                    sheet1.Cells[1, 8].Value = "留在上海数量";
+                    sheet1.Cells[1, 9].Value = "移往昆山数量";
+                    #endregion
+
+                    #region 数据行
+                    for (int idx = 0, rowIdx = 2, len = list.Count; idx < len; idx++, rowIdx++)
+                    {
+                        var info = list[idx];
+                        sheet1.Cells[rowIdx, 1].Value = info._SKU;
+                        sheet1.Cells[rowIdx, 2].Value = info._商品名称;
+                        sheet1.Cells[rowIdx, 3].Value = info._库位;
+                        sheet1.Cells[rowIdx, 4].Value = info._仓库;
+                        sheet1.Cells[rowIdx, 5].Value = info._货架;
+                        sheet1.Cells[rowIdx, 6].Value = info._库存数量;
+                        sheet1.Cells[rowIdx, 7].Value = info._盘点数量;
+                        sheet1.Cells[rowIdx, 8].Value = info._留在上海数量;
+                        sheet1.Cells[rowIdx, 9].Value = info._移往昆山数量;
+                    }
+                    #endregion
+
+                }
+                #endregion
+
+                #region 分表
+                {
+                    var storeNames = list.Select(x => x._仓库).Distinct().ToList();
+                    storeNames.ForEach(storeName =>
+                    {
+                        var sheet1 = workbox.Worksheets.Add(!string.IsNullOrEmpty(storeName) ? storeName : "空仓库");
+
+                        #region 标题行
+                        sheet1.Cells[1, 1].Value = "SKU";
+                        sheet1.Cells[1, 2].Value = "商品名称";
+                        sheet1.Cells[1, 3].Value = "库位";
+                        sheet1.Cells[1, 4].Value = "仓库";
+                        sheet1.Cells[1, 5].Value = "货架";
+                        sheet1.Cells[1, 6].Value = "库存数量";
+                        sheet1.Cells[1, 7].Value = "盘点数量";
+                        sheet1.Cells[1, 8].Value = "留在上海数量";
+                        sheet1.Cells[1, 9].Value = "移往昆山数量";
+                        #endregion
+
+                        #region 数据行
+                        var refList = list.Where(x => x._仓库 == storeName).ToList();
+                        for (int idx = 0, rowIdx = 2, len = refList.Count; idx < len; idx++, rowIdx++)
+                        {
+                            var info = refList[idx];
+                            sheet1.Cells[rowIdx, 1].Value = info._SKU;
+                            sheet1.Cells[rowIdx, 2].Value = info._商品名称;
+                            sheet1.Cells[rowIdx, 3].Value = info._库位;
+                            sheet1.Cells[rowIdx, 4].Value = info._仓库;
+                            sheet1.Cells[rowIdx, 5].Value = info._货架;
+                            sheet1.Cells[rowIdx, 6].Value = info._库存数量;
+                            sheet1.Cells[rowIdx, 7].Value = info._盘点数量;
+                            sheet1.Cells[rowIdx, 8].Value = info._留在上海数量;
+                            sheet1.Cells[rowIdx, 9].Value = info._移往昆山数量;
+                        }
+                        #endregion
+                    });
+                }
+                #endregion
+
+                buffer = package.GetAsByteArray();
+            }
+            #endregion
+
+            InvokeMainForm((obj) =>
+            {
+                SaveFileDialog saveFile = new SaveFileDialog();
+                saveFile.Filter = "Excel 工作簿|*.xlsx";//设置文件类型
+                saveFile.Title = "导出数据";//设置标题
+                saveFile.AddExtension = true;//是否自动增加所辍名
+                saveFile.AutoUpgradeEnabled = true;//是否随系统升级而升级外观
+                if (saveFile.ShowDialog() == DialogResult.OK)//如果点的是确定就得到文件路径
+                {
+                    var FileName = saveFile.FileName;//得到文件路径   
+                    var saveFilName = Path.GetFileNameWithoutExtension(FileName);
+
+                    txtExport.Text = FileName;
+                    var len = buffer.Length;
+                    using (var fs = File.Create(FileName, len))
+                    {
+                        fs.Write(buffer, 0, len);
+                    }
+                    ShowMsg("表格生成完毕");
+                }
+            }, null);
+        }
+        #endregion
+
+        #region ShowMsg 消息提示
+        /// <summary>
+        /// 消息提示
+        /// </summary>
+        /// <param name="strMsg"></param>
+        private void ShowMsg(string strMsg)
+        {
+            if (this.InvokeRequired)
+            {
+                var act = new Action<string>(ShowMsg);
+                this.Invoke(act, strMsg);
+            }
+            else
+            {
+                this.lbMsg.Text = strMsg;
+            }
+        }
+        #endregion
+
+        #region InvokeMainForm 调用主线程
+        protected void InvokeMainForm(Action act)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(act);
+            }
+            else
+            {
+                act.Invoke();
+            }
+        }
+        protected void InvokeMainForm(Action<object> act, object obj)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(act, obj);
+            }
+            else
+            {
+                act.Invoke(obj);
+            }
+        }
+        #endregion
+
+        /**************** common class ****************/
+
+        [ExcelTable("库存明细")]
+        class _库存明细Mapping
+        {
+            private string orgSKU;
+            private string org库位;
+            private string org商品名称;
+
+            [ExcelColumn("SKU码")]
+            public string SKU
+            {
+                get
+                {
+                    return orgSKU;
+                }
+                set
+                {
+                    orgSKU = value != null ? value.ToString().Trim() : "";
+                }
+            }
+
+            [ExcelColumn("商品名称")]
+            public string _商品名称
+            {
+                get
+                {
+                    return org商品名称;
+                }
+                set
+                {
+                    org商品名称 = value != null ? value.ToString().Trim() : "";
+                }
+            }
+
+            [ExcelColumn("库位")]
+            public string _库位
+            {
+                get
+                {
+                    return org库位;
+                }
+                set
+                {
+                    org库位 = value != null ? value.ToString().Trim() : "";
+                }
+            }
+
+            [ExcelColumn("库存数量")]
+            public decimal _库存数量 { get; set; }
+
+            [ExcelColumn("30天销量")]
+            public decimal _30天销量 { get; set; }
+
+            [ExcelColumn("15天销量")]
+            public decimal _15天销量 { get; set; }
+
+            [ExcelColumn("5天销量")]
+            public decimal _5天销量 { get; set; }
+
+            public decimal _平均日销量
+            {
+                get
+                {
+                    return Math.Round((_30天销量 / 30 + _15天销量 / 15 + _5天销量 / 5) / 3, 0);
+                }
+            }
+
+        }
+
+        [ExcelTable("库存明细")]
+        class _盘点表Mapping
+        {
+            private string orgSKU;
+
+            [ExcelColumn("SKU码")]
+            public string SKU
+            {
+                get
+                {
+                    return orgSKU;
+                }
+                set
+                {
+                    orgSKU = value != null ? value.ToString().Trim() : "";
+                }
+            }
+
+            [ExcelColumn("数量")]
+            public decimal _数量 { get; set; }
+        }
+
+        class _分析结果Model
+        {
+            public string _SKU { get; set; }
+            public string _商品名称 { get; set; }
+            public string _库位 { get; set; }
+            public decimal _库存数量 { get; set; }
+            public decimal _盘点数量 { get; set; }
+
+            public decimal _遗留上海天数 { get; set; }
+            public decimal _日平均销量 { get; set; }
+
+            public string _仓库
+            {
+                get
+                {
+                    var tmp = "";
+                    if (!string.IsNullOrEmpty(_库位))
+                    {
+                        tmp = _库位.Substring(0, 2);
+                    }
+                    return tmp;
+                }
+            }
+
+            public string _货架
+            {
+                get
+                {
+                    var tmp = "";
+                    if (!string.IsNullOrEmpty(_库位))
+                    {
+                        tmp = _库位.Substring(0, 3);
+                    }
+                    return tmp;
+                }
+            }
+
+            public decimal _移往昆山数量
+            {
+                get
+                {
+                    decimal tmp = 0;
+                    if (_盘点数量 != 0)
+                    {
+                        tmp = _盘点数量 - _留在上海数量;
+                    }
+                    else
+                    {
+                        tmp = _库存数量 - _留在上海数量;
+                    }
+                    return tmp;
+                }
+            }
+
+            public decimal _留在上海数量
+            {
+                get
+                {
+                    return Math.Round(_遗留上海天数 * _日平均销量, 0);
+                }
+            }
+        }
+
+
+    }
+}
