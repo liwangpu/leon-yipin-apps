f112582d893c0417a0f71fb529afbb5a019b1614
LeonPu2017 <liwangp2012@163.com>
2017-12-27 22:25:33 +0800

    finish product culcu

--- YiPin_Apps/Gadget/_产品销量统计.Designer.cs
+++ YiPin_Apps/Gadget/_产品销量统计.Designer.cs
@@ -29,32 +29,32 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nud爆款量 = new System.Windows.Forms.NumericUpDown();
+            this.nud滞销量 = new System.Windows.Forms.NumericUpDown();
+            this.dtp开发截止时间 = new System.Windows.Forms.DateTimePicker();
+            this.dtp开发起始时间 = new System.Windows.Forms.DateTimePicker();
             this.txt停售商品信息 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.lkDecs = new System.Windows.Forms.LinkLabel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btnAnalyze = new System.Windows.Forms.Button();
             this.lbMsg = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.btn停售商品信息 = new System.Windows.Forms.Button();
+            this.btn各平台销量一览表 = new System.Windows.Forms.Button();
+            this.txt各平台销量一览表 = new System.Windows.Forms.TextBox();
             this.btn在售商品信息 = new System.Windows.Forms.Button();
             this.txt在售商品信息 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txt各平台销量一览表 = new System.Windows.Forms.TextBox();
-            this.btn各平台销量一览表 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.dtp开发起始时间 = new System.Windows.Forms.DateTimePicker();
-            this.label7 = new System.Windows.Forms.Label();
-            this.dtp开发截止时间 = new System.Windows.Forms.DateTimePicker();
-            this.label8 = new System.Windows.Forms.Label();
-            this.nud滞销量 = new System.Windows.Forms.NumericUpDown();
-            this.label9 = new System.Windows.Forms.Label();
-            this.nud爆款量 = new System.Windows.Forms.NumericUpDown();
-            this.label10 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nud滞销量)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nud爆款量)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud滞销量)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -89,6 +89,59 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "数据上传";
             // 
+            // nud爆款量
+            // 
+            this.nud爆款量.Location = new System.Drawing.Point(242, 179);
+            this.nud爆款量.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nud爆款量.Minimum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.nud爆款量.Name = "nud爆款量";
+            this.nud爆款量.Size = new System.Drawing.Size(120, 21);
+            this.nud爆款量.TabIndex = 25;
+            this.nud爆款量.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // nud滞销量
+            // 
+            this.nud滞销量.Location = new System.Drawing.Point(61, 180);
+            this.nud滞销量.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nud滞销量.Name = "nud滞销量";
+            this.nud滞销量.Size = new System.Drawing.Size(120, 21);
+            this.nud滞销量.TabIndex = 25;
+            this.nud滞销量.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // dtp开发截止时间
+            // 
+            this.dtp开发截止时间.Location = new System.Drawing.Point(242, 129);
+            this.dtp开发截止时间.Name = "dtp开发截止时间";
+            this.dtp开发截止时间.Size = new System.Drawing.Size(122, 21);
+            this.dtp开发截止时间.TabIndex = 24;
+            // 
+            // dtp开发起始时间
+            // 
+            this.dtp开发起始时间.Location = new System.Drawing.Point(61, 129);
+            this.dtp开发起始时间.Name = "dtp开发起始时间";
+            this.dtp开发起始时间.Size = new System.Drawing.Size(122, 21);
+            this.dtp开发起始时间.TabIndex = 24;
+            // 
             // txt停售商品信息
             // 
             this.txt停售商品信息.Enabled = false;
@@ -96,6 +149,24 @@
             this.txt停售商品信息.Name = "txt停售商品信息";
             this.txt停售商品信息.Size = new System.Drawing.Size(241, 21);
             this.txt停售商品信息.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(191, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "截止";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(189, 181);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 12);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "爆款量:";
             // 
             // lkDecs
             // 
@@ -108,6 +179,42 @@
             this.lkDecs.Text = "表格说明";
             this.lkDecs.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lkDecs_LinkClicked);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 182);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 12);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "滞销量:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "起始:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 158);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 12);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "滞销爆款量:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "产品开发时间:";
+            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -116,6 +223,15 @@
             this.label6.Size = new System.Drawing.Size(83, 12);
             this.label6.TabIndex = 6;
             this.label6.Text = "停售商品信息:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "各平台销量一览表:";
             // 
             // label4
             // 
@@ -164,6 +280,24 @@
             this.btn停售商品信息.UseVisualStyleBackColor = true;
             this.btn停售商品信息.Click += new System.EventHandler(this.btn停售商品信息_Click);
             // 
+            // btn各平台销量一览表
+            // 
+            this.btn各平台销量一览表.Location = new System.Drawing.Point(370, 76);
+            this.btn各平台销量一览表.Name = "btn各平台销量一览表";
+            this.btn各平台销量一览表.Size = new System.Drawing.Size(75, 23);
+            this.btn各平台销量一览表.TabIndex = 1;
+            this.btn各平台销量一览表.Text = "浏览";
+            this.btn各平台销量一览表.UseVisualStyleBackColor = true;
+            this.btn各平台销量一览表.Click += new System.EventHandler(this.btn各平台销量一览表_Click);
+            // 
+            // txt各平台销量一览表
+            // 
+            this.txt各平台销量一览表.Enabled = false;
+            this.txt各平台销量一览表.Location = new System.Drawing.Point(123, 78);
+            this.txt各平台销量一览表.Name = "txt各平台销量一览表";
+            this.txt各平台销量一览表.Size = new System.Drawing.Size(241, 21);
+            this.txt各平台销量一览表.TabIndex = 0;
+            // 
             // btn在售商品信息
             // 
             this.btn在售商品信息.Location = new System.Drawing.Point(370, 18);
@@ -182,153 +316,21 @@
             this.txt在售商品信息.Size = new System.Drawing.Size(241, 21);
             this.txt在售商品信息.TabIndex = 0;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 81);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 12);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "各平台销量一览表:";
-            // 
-            // txt各平台销量一览表
-            // 
-            this.txt各平台销量一览表.Enabled = false;
-            this.txt各平台销量一览表.Location = new System.Drawing.Point(123, 78);
-            this.txt各平台销量一览表.Name = "txt各平台销量一览表";
-            this.txt各平台销量一览表.Size = new System.Drawing.Size(241, 21);
-            this.txt各平台销量一览表.TabIndex = 0;
-            // 
-            // btn各平台销量一览表
-            // 
-            this.btn各平台销量一览表.Location = new System.Drawing.Point(370, 76);
-            this.btn各平台销量一览表.Name = "btn各平台销量一览表";
-            this.btn各平台销量一览表.Size = new System.Drawing.Size(75, 23);
-            this.btn各平台销量一览表.TabIndex = 1;
-            this.btn各平台销量一览表.Text = "浏览";
-            this.btn各平台销量一览表.UseVisualStyleBackColor = true;
-            this.btn各平台销量一览表.Click += new System.EventHandler(this.btn各平台销量一览表_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 110);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 12);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "产品开发时间:";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(20, 135);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 12);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "起始:";
-            // 
-            // dtp开发起始时间
-            // 
-            this.dtp开发起始时间.Location = new System.Drawing.Point(61, 129);
-            this.dtp开发起始时间.Name = "dtp开发起始时间";
-            this.dtp开发起始时间.Size = new System.Drawing.Size(122, 21);
-            this.dtp开发起始时间.TabIndex = 24;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(191, 135);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(29, 12);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "截止";
-            // 
-            // dtp开发截止时间
-            // 
-            this.dtp开发截止时间.Location = new System.Drawing.Point(242, 129);
-            this.dtp开发截止时间.Name = "dtp开发截止时间";
-            this.dtp开发截止时间.Size = new System.Drawing.Size(122, 21);
-            this.dtp开发截止时间.TabIndex = 24;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(8, 182);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(47, 12);
-            this.label8.TabIndex = 6;
-            this.label8.Text = "滞销量:";
-            // 
-            // nud滞销量
-            // 
-            this.nud滞销量.Location = new System.Drawing.Point(61, 180);
-            this.nud滞销量.Maximum = new decimal(new int[] {
-            9999,
-            0,
-            0,
-            0});
-            this.nud滞销量.Name = "nud滞销量";
-            this.nud滞销量.Size = new System.Drawing.Size(120, 21);
-            this.nud滞销量.TabIndex = 25;
-            this.nud滞销量.Value = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(189, 181);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(47, 12);
-            this.label9.TabIndex = 6;
-            this.label9.Text = "爆款量:";
-            // 
-            // nud爆款量
-            // 
-            this.nud爆款量.Location = new System.Drawing.Point(242, 179);
-            this.nud爆款量.Maximum = new decimal(new int[] {
-            9999,
-            0,
-            0,
-            0});
-            this.nud爆款量.Minimum = new decimal(new int[] {
-            300,
-            0,
-            0,
-            0});
-            this.nud爆款量.Name = "nud爆款量";
-            this.nud爆款量.Size = new System.Drawing.Size(120, 21);
-            this.nud爆款量.TabIndex = 25;
-            this.nud爆款量.Value = new decimal(new int[] {
-            300,
-            0,
-            0,
-            0});
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(6, 158);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(71, 12);
-            this.label10.TabIndex = 6;
-            this.label10.Text = "滞销爆款量:";
-            // 
             // _产品销量统计
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(475, 256);
             this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(491, 294);
+            this.MinimumSize = new System.Drawing.Size(491, 294);
             this.Name = "_产品销量统计";
             this.Text = "_产品销量统计";
             this.Load += new System.EventHandler(this._产品销量统计_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nud滞销量)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nud爆款量)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud滞销量)).EndInit();
             this.ResumeLayout(false);
 
         }
