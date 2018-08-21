03e183d93aecec8289d656d53c5a0b19ea99194f
leon-github <liwang.pu@gmail.com>
2018-08-21 13:38:25 +0800

    完成退款表格合并(添加交易数量汇总)

--- YiPin_Apps/Gadget/_退款汇总表格合并.Designer.cs
+++ YiPin_Apps/Gadget/_退款汇总表格合并.Designer.cs
@@ -33,13 +33,14 @@
             this.btn合并 = new System.Windows.Forms.Button();
             this.lbMsg = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.btn交易数量汇总 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn浏览
             // 
-            this.btn浏览.Location = new System.Drawing.Point(75, 89);
+            this.btn浏览.Location = new System.Drawing.Point(12, 89);
             this.btn浏览.Name = "btn浏览";
-            this.btn浏览.Size = new System.Drawing.Size(75, 23);
+            this.btn浏览.Size = new System.Drawing.Size(106, 23);
             this.btn浏览.TabIndex = 0;
             this.btn浏览.Text = "浏览";
             this.btn浏览.UseVisualStyleBackColor = true;
@@ -56,18 +57,18 @@
             // 
             // btn合并
             // 
-            this.btn合并.Location = new System.Drawing.Point(156, 89);
+            this.btn合并.Location = new System.Drawing.Point(124, 89);
             this.btn合并.Name = "btn合并";
-            this.btn合并.Size = new System.Drawing.Size(75, 23);
+            this.btn合并.Size = new System.Drawing.Size(107, 23);
             this.btn合并.TabIndex = 2;
-            this.btn合并.Text = "处理";
+            this.btn合并.Text = "合并汇总";
             this.btn合并.UseVisualStyleBackColor = true;
             this.btn合并.Click += new System.EventHandler(this.btn合并_Click);
             // 
             // lbMsg
             // 
             this.lbMsg.AutoSize = true;
-            this.lbMsg.Location = new System.Drawing.Point(53, 121);
+            this.lbMsg.Location = new System.Drawing.Point(53, 153);
             this.lbMsg.Name = "lbMsg";
             this.lbMsg.Size = new System.Drawing.Size(65, 12);
             this.lbMsg.TabIndex = 3;
@@ -76,25 +77,36 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 121);
+            this.label1.Location = new System.Drawing.Point(12, 153);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 3;
             this.label1.Text = "状态:";
             // 
+            // btn交易数量汇总
+            // 
+            this.btn交易数量汇总.Location = new System.Drawing.Point(124, 119);
+            this.btn交易数量汇总.Name = "btn交易数量汇总";
+            this.btn交易数量汇总.Size = new System.Drawing.Size(107, 23);
+            this.btn交易数量汇总.TabIndex = 4;
+            this.btn交易数量汇总.Text = "交易数量汇总";
+            this.btn交易数量汇总.UseVisualStyleBackColor = true;
+            this.btn交易数量汇总.Click += new System.EventHandler(this.btn交易数量汇总_Click);
+            // 
             // _退款汇总表格合并
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(243, 142);
+            this.ClientSize = new System.Drawing.Size(249, 174);
+            this.Controls.Add(this.btn交易数量汇总);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbMsg);
             this.Controls.Add(this.btn合并);
             this.Controls.Add(this.txtPath);
             this.Controls.Add(this.btn浏览);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(259, 181);
-            this.MinimumSize = new System.Drawing.Size(259, 181);
+            this.MaximumSize = new System.Drawing.Size(265, 213);
+            this.MinimumSize = new System.Drawing.Size(265, 213);
             this.Name = "_退款汇总表格合并";
             this.Text = "退款汇总表格合并";
             this.ResumeLayout(false);
@@ -109,5 +121,6 @@
         private System.Windows.Forms.Button btn合并;
         private System.Windows.Forms.Label lbMsg;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn交易数量汇总;
     }
 }
--- YiPin_Apps/Gadget/_退款汇总表格合并.cs
+++ YiPin_Apps/Gadget/_退款汇总表格合并.cs
@@ -34,7 +34,7 @@ namespace Gadget
         }
         #endregion
 
-        #region 数据处理
+        #region 合并汇总
         private void btn合并_Click(object sender, EventArgs e)
         {
             var pathStr = txtPath.Text;
@@ -96,45 +96,84 @@ namespace Gadget
                         }
                     }
 
-
-                    //var list卖家简称s = list.Select(x => x._卖家简称).Distinct().ToList();
-                    //var list物流方式s = list.Select(x => x._物流方式).Distinct().ToList();
-                    //var list月份s = list.Select(x => x._月份).Distinct().OrderBy(x => x).ToList();
-                    //var list国家s = list.Select(x => x._交易国家).Distinct().OrderBy(x => x).ToList();
-                    //foreach (var _卖家简称s in list卖家简称s)
-                    //{
-                    //    foreach (var _物流方式s in list物流方式s)
-                    //    {
-                    //        foreach (var _月份s in list月份s)
-                    //        {
-                    //            foreach (var _国家s in list国家s)
-                    //            {
-                    //                var refers = list.Where(x => x._卖家简称 == _卖家简称s && x._物流方式 == _物流方式s && x._月份 == _月份s && x._交易国家 == _国家s).ToList();
-                    //                if (refers.Count > 0)
-                    //                {
-                    //                    var mode = new _统计结果();
-                    //                    mode._卖家简称 = _卖家简称s;
-                    //                    mode._物流方式 = _物流方式s;
-                    //                    mode._月份 = _月份s;
-                    //                    mode._交易国家 = _国家s;
-                    //                    mode._发货 = refers.Count;
-                    //                    mode._退款 = refers.Where(x => x._已退款 == 1).Count();
-                    //                    result.Add(mode);
-                    //                }
-                    //            }
-                    //        }
-                    //    }
-                    //}
                     ShowMsg("即将导出表格");
-                    Export(result);
+                    Export_合并汇总(result);
                 }, null);
                 #endregion
             }
         }
         #endregion
 
-        #region 导出表格
-        private void Export(List<_统计结果> list)
+        #region 交易数量汇总
+        private void btn交易数量汇总_Click(object sender, EventArgs e)
+        {
+            var pathStr = txtPath.Text;
+            if (!string.IsNullOrWhiteSpace(pathStr))
+            {
+                btn浏览.Enabled = false;
+                btn合并.Enabled = false;
+                var paths = pathStr.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                var list = new List<_退款数据>();
+                var result = new List<_交易数量统计结果>();
+
+                #region 读取数据
+                var actReadData = new Action(() =>
+                {
+                    ShowMsg("正在读取数据,请稍等");
+                    foreach (var path in paths)
+                    {
+                        using (var excel = new ExcelQueryFactory(path))
+                        {
+                            var qs = from c in excel.Worksheet<_退款数据>()
+                                     select c;
+                            list.AddRange(qs);
+                        }
+                    }
+                });
+                #endregion
+
+                #region 处理数据
+                actReadData.BeginInvoke((obj) =>
+                {
+                    ShowMsg("正在处理数据,请稍等");
+
+                    foreach (var item in list)
+                    {
+                        var bExist = false;
+                        for (int idx = result.Count - 1; idx >= 0; idx--)
+                        {
+                            var rsItem = result[idx];
+                            if (rsItem._卖家简称 == item._卖家简称 && rsItem._交易时间 == item._交易日期)
+                            {
+                                rsItem._发货数量++;
+                                if (item._已退款)
+                                    rsItem._退款数量++;
+                                bExist = true;
+                                break;
+                            }
+                        }
+
+                        if (!bExist)
+                        {
+                            var mode = new _交易数量统计结果();
+                            mode._卖家简称 = item._卖家简称;
+                            mode._交易时间 = item._交易日期;
+                            mode._发货数量 = 1;
+                            mode._退款数量 = item._已退款 ? 1 : 0;
+                            result.Add(mode);
+                        }
+                    }
+
+                    ShowMsg("即将导出表格");
+                    Export_交易数量汇总(result.OrderBy(x => x._交易时间).ToList());
+                }, null);
+                #endregion
+            }
+        } 
+        #endregion
+
+        #region Export_合并汇总 导出合并汇总
+        private void Export_合并汇总(List<_统计结果> list)
         {
             var buffer = new byte[0];
             #region 订单分配
@@ -223,6 +262,93 @@ namespace Gadget
                 }
             }, null);
         }
+        #endregion
+
+        #region Export_交易数量汇总 导出交易数量汇总
+        private void Export_交易数量汇总(List<_交易数量统计结果> list)
+        {
+            var buffer = new byte[0];
+            #region 订单分配
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                var workbox = package.Workbook;
+                var sheet1 = workbox.Worksheets.Add("Sheet1");
+
+                #region 标题行
+                sheet1.Cells[1, 1].Value = "卖家简称";
+                sheet1.Cells[1, 2].Value = "交易时间";
+                sheet1.Cells[1, 3].Value = "发货总量";
+                sheet1.Cells[1, 4].Value = "退货总量";
+
+
+
+                #endregion
+
+                #region 数据行
+                for (int idx = 0, rowIdx = 2, len = list.Count; idx < len; idx++)
+                {
+                    var curOrder = list[idx];
+                    sheet1.Cells[rowIdx, 1].Value = curOrder._卖家简称;
+                    sheet1.Cells[rowIdx, 2].Value = curOrder._交易时间;
+                    sheet1.Cells[rowIdx, 3].Value = curOrder._发货数量;
+                    sheet1.Cells[rowIdx, 4].Value = curOrder._退款数量;
+
+                    rowIdx++;
+                }
+                #endregion
+
+                #region 全部边框
+                {
+                    var endRow = sheet1.Dimension.End.Row;
+                    var endColumn = sheet1.Dimension.End.Column;
+                    using (var rng = sheet1.Cells[1, 1, endRow, endColumn])
+                    {
+                        rng.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                        rng.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                        rng.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                        rng.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                    }
+                }
+                #endregion
+
+                sheet1.Cells[sheet1.Dimension.Address].AutoFitColumns();
+
+                buffer = package.GetAsByteArray();
+            }
+            #endregion
+
+            InvokeMainForm((obj) =>
+            {
+
+                SaveFileDialog saveFile = new SaveFileDialog();
+                saveFile.Filter = "Excel 工作簿|*.xlsx";//设置文件类型
+                saveFile.Title = "导出数据";//设置标题
+                saveFile.AddExtension = true;//是否自动增加所辍名
+                saveFile.AutoUpgradeEnabled = true;//是否随系统升级而升级外观
+                if (saveFile.ShowDialog() == DialogResult.OK)//如果点的是确定就得到文件路径
+                {
+                    var FileName = saveFile.FileName;//得到文件路径   
+                    var saveFilName = Path.GetFileNameWithoutExtension(FileName);
+                    var savePath = Path.GetDirectoryName(FileName);
+
+                    try
+                    {
+                        var len = buffer.Length;
+                        using (var fs = File.Create(FileName, len))
+                        {
+                            fs.Write(buffer, 0, len);
+                        }
+                        ShowMsg("表格导出完毕");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "温馨提示");
+                    }
+                    btn浏览.Enabled = true;
+                    btn合并.Enabled = true;
+                }
+            }, null);
+        } 
         #endregion
 
         #region InvokeMainForm 调用主线程
@@ -331,6 +457,13 @@ namespace Gadget
                     return _交易时间.Month;
                 }
             }
+            public string _交易日期
+            {
+                get
+                {
+                    return _交易时间.ToString("yyyy-MM-dd");
+                }
+            }
         }
 
         class _统计结果
@@ -351,6 +484,14 @@ namespace Gadget
 
                 }
             }
+        }
+
+        class _交易数量统计结果
+        {
+            public string _卖家简称 { get; set; }
+            public string _交易时间 { get; set; }
+            public int _发货数量 { get; set; }
+            public int _退款数量 { get; set; }
         }
     }
 }
