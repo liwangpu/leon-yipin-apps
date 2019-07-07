21ce85437ab459cf6af36c1063acaf87b2661b30
leon-github <liwang.pu@gmail.com>
2019-07-07 14:28:10 +0800

    换系统前push

--- YiPin_Apps/Gadget/_快速提取不同库位子SKU.cs
+++ YiPin_Apps/Gadget/_快速提取不同库位子SKU.cs
@@ -418,6 +418,11 @@ namespace Gadget
                             }
                             if (errorSkus.Count == 0)
                                 continue;
+
+
+                            if (errorSkus.Select(x=>x.Area).Distinct().Count()==1)
+                                continue;
+
                             //父SKU
                             using (var rng = sheet1.Cells[currentRow, 1, currentRow + errorSkus.Count - 1, 1])
                             {
@@ -490,75 +495,6 @@ namespace Gadget
             }, null);
 
         }
-        //private void Export(List<_在售SKUMapping> _在售List)
-        //{
-        //    ShowMsg("开始生成表格");
-        //    var buffer1 = new byte[0];
-
-        //    #region 生成表
-        //    using (ExcelPackage package = new ExcelPackage())
-        //    {
-        //        var workbox = package.Workbook;
-
-        //        #region 在售
-        //        {
-        //            var sheet1 = workbox.Worksheets.Add("在售");
-
-        //            #region 标题行
-        //            sheet1.Cells[1, 1].Value = "SKU";
-        //            sheet1.Cells[1, 2].Value = "商品名称";
-        //            sheet1.Cells[1, 3].Value = "上海库位";
-        //            sheet1.Cells[1, 4].Value = "上海仓库";
-        //            sheet1.Cells[1, 5].Value = "上海货架";
-        //            sheet1.Cells[1, 6].Value = "昆山库位";
-        //            sheet1.Cells[1, 7].Value = "昆山区域";
-        //            sheet1.Cells[1, 8].Value = "可用数量";
-        //            sheet1.Cells[1, 9].Value = "备注";
-        //            #endregion
-
-        //            #region 数据行
-        //            for (int idx = 0, rowIdx = 2, len = _在售List.Count; idx < len; idx++, rowIdx++)
-        //            {
-        //                var info = _在售List[idx];
-        //                sheet1.Cells[rowIdx, 1].Value = info._SKU;
-        //                sheet1.Cells[rowIdx, 2].Value = info._商品名称;
-        //                sheet1.Cells[rowIdx, 3].Value = info._上海库位;
-        //                sheet1.Cells[rowIdx, 4].Value = info._上海仓库;
-        //                sheet1.Cells[rowIdx, 5].Value = info._上海货架;
-        //                sheet1.Cells[rowIdx, 6].Value = info._昆山库位;
-        //                sheet1.Cells[rowIdx, 7].Value = info._昆山区域;
-        //                sheet1.Cells[rowIdx, 8].Value = info._可用数量;
-        //                sheet1.Cells[rowIdx, 9].Value = info._备注;
-        //            }
-        //            #endregion
-
-        //        }
-        //        #endregion
-
-        //        buffer1 = package.GetAsByteArray();
-        //    }
-        //    #endregion
-
-        //    InvokeMainForm((obj) =>
-        //    {
-        //        SaveFileDialog saveFile = new SaveFileDialog();
-        //        saveFile.Filter = "Excel 工作簿|*.xlsx";//设置文件类型
-        //        saveFile.Title = "导出数据";//设置标题
-        //        saveFile.AddExtension = true;//是否自动增加所辍名
-        //        saveFile.AutoUpgradeEnabled = true;//是否随系统升级而升级外观
-        //        if (saveFile.ShowDialog() == DialogResult.OK)//如果点的是确定就得到文件路径
-        //        {
-        //            var FileName = saveFile.FileName;//得到文件路径   
-        //            var saveFilName = Path.GetFileNameWithoutExtension(FileName);
-        //            var len = buffer1.Length;
-        //            using (var fs = File.Create(FileName, len))
-        //            {
-        //                fs.Write(buffer1, 0, len);
-        //            }
-        //            ShowMsg("表格生成完毕");
-        //        }
-        //    }, null);
-        //}
         #endregion
 
         #region ShowMsg 消息提示
